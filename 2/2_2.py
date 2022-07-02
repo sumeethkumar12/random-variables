84a8ef485b38fca019ce7b3757bb12be9d883785
sumeethkumar12 <102542447+sumeethkumar12@users.noreply.github.com>
2022-07-02 12:57:56 +0530

    Create 2_2.py

--- 2/2_2.py
+++ 2/2_2.py
@@ -1,24 +1,31 @@
 
+#
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-
-
+def q(x):
+    return(math.erfc(x/math.sqrt(2)))/2.0
+     
 
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
+err2=[]
 #randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('gau.dat',dtype='double')
+
 #randvar = np.loadtxt('gau.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
-
-	
-plt.plot(x.T,err)#plotting the CDF
+for i in range(len(x)):
+    c=q(-x[i])
+    err2.append(c)
+plt.plot(x.T,err,'o')	
+plt.plot(x.T,err2)#plotting the CDF
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
